27dfeeb4b45191b23d32b1fdee384647ece75156
jkaplanis <johntkaplanis@gmail.com>
2020-09-14 22:42:31 -0700

    first commit

---
+++ email.js
@@ -0,0 +1,20 @@
+function sendEmail() {
+  var emailsSent = 0;
+  var emailInterval = setInterval(function () {
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "johntkaplanis@gmail.com",
+      Password: "password",
+      To: "jkap07@yahoo.com",
+      From: "johntkaplanis@gmail.com",
+      Subject: "This is a test",
+      Body: "If you are reading this then the test worked."
+    }).then(message => {
+      emailsSent += 1;
+      // clearInterval after the function has been called 3 times.
+      if (emailsSent === 2) {
+        clearInterval(emailInterval);
+      }
+    });
+  }, 15000);
+}
