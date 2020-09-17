6a014c074af3f8915b3849efb216100d1e6ddf1b
jkaplanis <johntkaplanis@gmail.com>
2020-09-16 21:32:59 -0700

    add function to retrieve input values

--- email.js
+++ email.js
@@ -1,18 +1,46 @@
-function sendEmail() {
+function getValues() {
+  var recipientEmail = document.getElementById("recipientEmail").value;
+  var senderEmail = document.getElementById("senderEmail").value;
+  var senderPassword = document.getElementById("senderPassword").value;
+  var emailSubject = document.getElementById("emailSubject").value;
+  var emailBody = document.getElementById("emailBody").value;
+  var numberOfEmails = document.getElementById("numberOfEmails").value;
+  var emailInterval = document.getElementById("emailInterval").value * 60000;
+
+  sendEmail(
+    recipientEmail,
+    senderEmail,
+    senderPassword,
+    emailSubject,
+    emailBody,
+    numberOfEmails,
+    emailInterval
+  );
+}
+
+function sendEmail(
+  recipientEmail,
+  senderEmail,
+  senderPassword,
+  emailSubject,
+  emailBody,
+  numberOfEmails,
+  emailInterval
+) {
   var emailsSent = 0;
   var emailInterval = setInterval(function () {
     Email.send({
       Host: "smtp.gmail.com",
-      Username: "johntkaplanis@gmail.com",
-      Password: "password",
-      To: "jkap07@yahoo.com",
-      From: "johntkaplanis@gmail.com",
-      Subject: "This is a test",
-      Body: "If you are reading this then the test worked."
+      Username: `${senderEmail}`,
+      Password: `${senderPassword}`,
+      To: `${recipientEmail}`,
+      From: `${senderEmail}`,
+      Subject: `${emailSubject}`,
+      Body: `${emailBody}`
     }).then(message => {
       emailsSent += 1;
       // clearInterval after the function has been called 3 times.
-      if (emailsSent === 2) {
+      if (emailsSent === numberOfEmails) {
         clearInterval(emailInterval);
       }
     });
